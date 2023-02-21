4741715fab31d2bacdc979c346e34b6a426f2c96
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-02-21 18:52:34 +0100

    milestone 2 completata

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function generatePassword($length)
+{
+    $passwordLength = $length;
+
+    $characters = "abcdefghijlmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ0123456789_?*+&%!#@";
+    $charactersLength = strlen($characters);
+
+    $randomPassword = "";
+    $i = 0;
+
+    while ($i < $length) {
+        $randomNumber = rand(0, $charactersLength - 1);
+        $randomPassword .= $characters[$randomNumber];
+        $i++;
+    }
+    return $randomPassword;
+}
